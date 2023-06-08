c0436fa62ac831674ceb71d853bd3d95d9d403bb
PerterParker1 chao <1578486312@qq.com>
2023-06-08 15:49:50 +0000

    save

--- qiujiepingfanggen.py
+++ qiujiepingfanggen.py
@@ -1,5 +1,5 @@
 S=float(input("请输入你要求解平方根的正数："))
-infinitesimal = 0.00000000000000000000000000000000000001
+infinitesimal = 0.00000000000000000000001
 
 a = S / 2
 
@@ -7,7 +7,9 @@ counter = 1
 while True:
     b = S / a
     delta = a - b
-    print(f'这是第{counter}轮，S={S}, a={a}, b={b}, delta={delta}, 是否结束：{delta < infinitesimal}')
+    c = round(b,2)
+    d = round(b,2)
+    print(f'这是第{counter}轮，S={S}, a={c}, b={d}, delta={delta}, 是否结束：{delta < infinitesimal}')
     
     counter = counter + 1
 
